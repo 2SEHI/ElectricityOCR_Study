5acf4229f4f8ad3d3e642e5ffd6eca494412173b
2SEHI <58774664+2SEHI@users.noreply.github.com>
2021-09-30 16:02:43 +0900

    sql 수정

--- db/CreateTableSQL.sql
+++ db/CreateTableSQL.sql
@@ -33,135 +33,75 @@ grant all privileges on electricityDB.* to 'flaskServer'@'localhost';
 SHOW GRANTS FOR 'flaskServer'@localhost;
 
 
-CREATE TABLE `MY_SCHEMA`.`electricity_meter_tb` (
-	`serial_cd`             VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`supply_type`           VARCHAR(30)  NULL     COMMENT 'supply_type',
-	`typename`              VARCHAR(6)   NULL     COMMENT 'typename',
-	`electricity_filename`  VARCHAR(100) NOT NULL COMMENT 'electricity_filename',
-	`region_cd`             VARCHAR(20)  NULL     COMMENT 'region_cd',
-	`electricity_save_date` DATE         NOT NULL COMMENT 'electricity_save_date',
-	`del_flag`              VARCHAR(1)   NOT NULL COMMENT 'del_flag'
-)
-COMMENT 'electricity_meter_tb'
-
-CREATE command denied to user 'flaskServer'@'localhost' for table 'electricity_meter_tb'
-ALTER TABLE `MY_SCHEMA`.`electricity_meter_tb`
-	ADD CONSTRAINT `PK_electricity_meter_tb`
-		PRIMARY KEY (
-			`serial_cd`
-		)
-
-ALTER command denied to user 'flaskServer'@'localhost' for table 'electricity_meter_tb'
-CREATE TABLE `MY_SCHEMA`.`modem_tb` (
-	`modem_cd`        VARCHAR(30)  NOT NULL COMMENT 'modem_cd',
-	`serial_cd`       VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`modem_filename`  VARCHAR(100) NOT NULL COMMENT 'modem_filename',
-	`modem_save_date` DATE         NOT NULL COMMENT 'modem_save_date'
-)
-COMMENT 'modem_tb'
-
-CREATE command denied to user 'flaskServer'@'localhost' for table 'modem_tb'
-ALTER TABLE `MY_SCHEMA`.`modem_tb`
-	ADD CONSTRAINT `PK_modem_tb`
-		PRIMARY KEY (
-			`modem_cd`,
-			`serial_cd`
-		)
-
-ALTER command denied to user 'flaskServer'@'localhost' for table 'modem_tb'
-CREATE TABLE `MY_SCHEMA`.`electricity_preprocessing_tb` (
-	`pre_id`       NUMERIC      NOT NULL COMMENT 'pre_id',
-	`serial_cd`    VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`pre_filename` VARCHAR(100) NOT NULL COMMENT 'pre_filename'
-)
-COMMENT 'electricity_preprocessing_tb'
-
-CREATE command denied to user 'flaskServer'@'localhost' for table 'electricity_preprocessing_tb'
-파일 또는 프로젝트가 삭제되어 에디터를 닫았습니다.
+-- electricity_meter_tb
 CREATE TABLE `electricityDB`.`electricity_meter_tb` (
-	`serial_cd`             VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`supply_type`           VARCHAR(30)  NULL     COMMENT 'supply_type',
-	`typename`              VARCHAR(6)   NULL     COMMENT 'typename',
-	`electricity_filename`  VARCHAR(100) NOT NULL COMMENT 'electricity_filename',
-	`region_cd`             VARCHAR(20)  NULL     COMMENT 'region_cd',
-	`electricity_save_date` DATE         NOT NULL COMMENT 'electricity_save_date',
-	`del_flag`              VARCHAR(1)   NOT NULL COMMENT 'del_flag'
+	`serial_cd`             VARCHAR(30)  NOT NULL COMMENT 'serial_cd', -- serial_cd
+	`supply_type`           VARCHAR(30)  NULL     COMMENT 'supply_type', -- supply_type
+	`typename`              VARCHAR(6)   NULL     COMMENT 'typename', -- typename
+	`electricity_filename`  VARCHAR(100) NOT NULL COMMENT 'electricity_filename', -- electricity_filename
+	`region_cd`             VARCHAR(20)  NULL     COMMENT 'region_cd', -- region_cd
+	`electricity_save_date` DATE         NOT NULL COMMENT 'electricity_save_date', -- electricity_save_date
+	`del_flag`              VARCHAR(1)   NOT NULL COMMENT 'del_flag' -- del_flag
 )
-COMMENT 'electricity_meter_tb'
+COMMENT 'electricity_meter_tb';
 
-DDL 구문이 수행되었습니다.
-
-
+-- electricity_meter_tb
 ALTER TABLE `electricityDB`.`electricity_meter_tb`
-	ADD CONSTRAINT `PK_electricity_meter_tb`
+	ADD CONSTRAINT `PK_electricity_meter_tb` -- electricity_meter_tb 기본키
 		PRIMARY KEY (
-			`serial_cd`
-		)
+			`serial_cd` -- serial_cd
+		);
 
-DDL 구문이 수행되었습니다.
-
-
+-- modem_tb
 CREATE TABLE `electricityDB`.`modem_tb` (
-	`modem_cd`        VARCHAR(30)  NOT NULL COMMENT 'modem_cd',
-	`serial_cd`       VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`modem_filename`  VARCHAR(100) NOT NULL COMMENT 'modem_filename',
-	`modem_save_date` DATE         NOT NULL COMMENT 'modem_save_date'
+	`modem_cd`        VARCHAR(30)  NOT NULL COMMENT 'modem_cd', -- modem_cd
+	`serial_cd`       VARCHAR(30)  NOT NULL COMMENT 'serial_cd', -- serial_cd
+	`modem_filename`  VARCHAR(100) NOT NULL COMMENT 'modem_filename', -- modem_filename
+	`modem_save_date` DATE         NOT NULL COMMENT 'modem_save_date' -- modem_save_date
 )
-COMMENT 'modem_tb'
+COMMENT 'modem_tb';
 
-DDL 구문이 수행되었습니다.
-
-
+-- modem_tb
 ALTER TABLE `electricityDB`.`modem_tb`
-	ADD CONSTRAINT `PK_modem_tb`
+	ADD CONSTRAINT `PK_modem_tb` -- modem_tb 기본키
 		PRIMARY KEY (
-			`modem_cd`,
-			`serial_cd`
-		)
+			`modem_cd`,  -- modem_cd
+			`serial_cd`  -- serial_cd
+		);
 
-DDL 구문이 수행되었습니다.
-
-
+-- electricity_preprocessing_tb
 CREATE TABLE `electricityDB`.`electricity_preprocessing_tb` (
-	`pre_id`       NUMERIC      NOT NULL COMMENT 'pre_id',
-	`serial_cd`    VARCHAR(30)  NOT NULL COMMENT 'serial_cd',
-	`modem_cd`     VARCHAR(30)  NOT NULL COMMENT 'modem_cd',
-	`pre_filename` VARCHAR(100) NOT NULL COMMENT 'pre_filename'
+	`pre_id`       NUMERIC      NOT NULL COMMENT 'pre_id', -- pre_id
+	`serial_cd`    VARCHAR(30)  NOT NULL COMMENT 'serial_cd', -- serial_cd
+	`modem_cd`     VARCHAR(30)  NOT NULL COMMENT 'modem_cd', -- modem_cd
+	`pre_filename` VARCHAR(100) NOT NULL COMMENT 'pre_filename' -- pre_filename
 )
-COMMENT 'electricity_preprocessing_tb'
+COMMENT 'electricity_preprocessing_tb';
 
-DDL 구문이 수행되었습니다.
-
-
+-- electricity_preprocessing_tb
 ALTER TABLE `electricityDB`.`electricity_preprocessing_tb`
-	ADD CONSTRAINT `PK_electricity_preprocessing_tb`
+	ADD CONSTRAINT `PK_electricity_preprocessing_tb` -- electricity_preprocessing_tb 기본키
 		PRIMARY KEY (
-			`pre_id`,
-			`serial_cd`
-		)
+			`pre_id`,    -- pre_id
+			`serial_cd`  -- serial_cd
+		);
 
-DDL 구문이 수행되었습니다.
-
-
+-- modem_tb
 ALTER TABLE `electricityDB`.`modem_tb`
-	ADD CONSTRAINT `FK_electricity_meter_tb_TO_modem_tb`
+	ADD CONSTRAINT `FK_electricity_meter_tb_TO_modem_tb` -- electricity_meter_tb -> modem_tb
 		FOREIGN KEY (
-			`serial_cd`
+			`serial_cd` -- serial_cd
 		)
-		REFERENCES `electricityDB`.`electricity_meter_tb` (
-			`serial_cd`
-		)
-DDL 구문이 수행되었습니다.
+		REFERENCES `electricityDB`.`electricity_meter_tb` ( -- electricity_meter_tb
+			`serial_cd` -- serial_cd
+		);
 
-
+-- electricity_preprocessing_tb
 ALTER TABLE `electricityDB`.`electricity_preprocessing_tb`
-	ADD CONSTRAINT `FK_electricity_meter_tb_TO_electricity_preprocessing_tb`
+	ADD CONSTRAINT `FK_electricity_meter_tb_TO_electricity_preprocessing_tb` -- electricity_meter_tb -> electricity_preprocessing_tb
 		FOREIGN KEY (
-			`serial_cd`
+			`serial_cd` -- serial_cd
 		)
-		REFERENCES `electricityDB`.`electricity_meter_tb` (
-			`serial_cd`
-		)
-DDL 구문이 수행되었습니다.
-
-
+		REFERENCES `electricityDB`.`electricity_meter_tb` ( -- electricity_meter_tb
+			`serial_cd` -- serial_cd
+		);
