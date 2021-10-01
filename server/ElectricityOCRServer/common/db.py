3211701f1cb1522c437991523fa7ce6763426245
2SEHI <58774664+2SEHI@users.noreply.github.com>
2021-10-01 11:09:38 +0900

    modem테이블도 조회하도록 select_all문을 수정

--- server/ElectricityOCRServer/common/db.py
+++ server/ElectricityOCRServer/common/db.py
@@ -6,17 +6,32 @@ class Dao:
 
     def __init__(self):
         self.__SELECT_ALL = '''
-        # select * from electricity_meter_tb
+        select electricity_meter_tb.serial_cd as serial_cd,
+        modem_tb.modem_cd as modem_cd ,
+        electricity_meter_tb.electricity_filename as electricity_filename, 
+        date_format(electricity_meter_tb.electricity_save_date, '%Y-%m-%d %H:%i:%s') as electricity_save_date 
+        from electricity_meter_tb 
+        join modem_tb 
+        on modem_tb.serial_cd = electricity_meter_tb.serial_cd 
+        where electricity_meter_tb.del_flag = 0
         '''
-        # self.__SELECT_ALL = '''
-        #     select electricity_meter_tb.serial_cd as serial_cd,
-        # 	electricity_meter_tb.typename as typename,
-        # 	electricity_meter_tb.electricity_save_date as electricity_save_date,
-        # 	modem_tb.modem_cd as modem_cd
-        #     from modem_tb
-        #     join electricity_meter_tb
-        #     on modem_tb.serial_cd = electricity_meter_tb.serial_cd;
-        #     '''
+
+        self.__SELECT_ONE = '''
+        select  
+        electricity_meter_tb.serial_cd as serial_cd,  
+        electricity_meter_tb.supply_type as supply_type, 
+        electricity_meter_tb.typename as typename, 
+        electricity_meter_tb.electricity_filename as electricity_filename, 
+        date_format(electricity_meter_tb.electricity_save_date, '%Y-%m-%d %H:%i:%s') as electricity_save_date,  
+        modem_tb.modem_cd as modem_cd, 
+        modem_tb.modem_filename as modem_filename 
+        from electricity_meter_tb 
+        join modem_tb 
+        on modem_tb.serial_cd = electricity_meter_tb.serial_cd  
+        where electricity_meter_tb.del_flag = 0 
+        and electricity_meter_tb.serial_cd = %s 
+        '''
+
     #  데이터베이스 연결 메소드
     def connect(self):
         # 연결
@@ -34,27 +49,24 @@ class Dao:
         self.con.close()
 
     # 전체 데이터 가져오기
-    def selectall(self):
+    def select_all(self):
         # 데이터베이스 연결
-        self.connect()
+
         li = []
         try:
-
+            self.connect()
             # sql 문 실행
             self.cursor.execute(self.__SELECT_ALL)
             data = self.cursor.fetchall()
             # 데이터를 저장할 list
 
             for temp in data:
-                dic = {}
-                dic['serial_cd'] = temp[0]
-                dic['supply_type'] = temp[1]
-                dic['typename'] = temp[2]
-                dic['electricity_filename'] = temp[3]
-                dic['region_cd'] = temp[4]
-                dic['electricity_save_date'] = temp[5]
-                dic['del_flag'] = temp[6]
-                li.append(dic)
+                item = {}
+                item['serial_cd'] = temp[0]
+                item['modem_cd'] = temp[1]
+                item['electricity_filename'] = temp[2]
+                item['electricity_save_date'] = temp[3]
+                li.append(item)
 
         except Exception as e:
             # insert 실패시 False 반환
@@ -62,24 +74,26 @@ class Dao:
         self.close()
         return li
 
-# dict형태로 데이터를 받아서 삽입하는 메소드
-def insertitem(self, item):
-    self.connect()
-    # 가장 큰 itemid를 가져와서 1을 더해서 itemid를 생성
-    self.cursor.execute('select max(itemid) from item')
-    data = self.cursor.fetchone()
-    # 데이터가 없어서 예외가 발생할 경우를 대비하여 try except문으로 예외처리를 해줍니다.
-    try:
-        itemid = int(data[0] + 1)
+    # dict형태로 데이터를 받아서 삽입하는 메소드
+    def select_one(self, serial_cd):
+        item = {}
+        try:
+            self.connect()
+            # 가장 큰 itemid를 가져와서 1을 더해서 itemid를 생성
+            self.cursor.execute(self.__SELECT_ONE, str(serial_cd))
+            data = self.cursor.fetchall()
+            print(data)
+            item['serial_cd'] = data[0]
+            item['supply_type'] = data[1]
+            item['typename'] = data[2]
+            item['electricity_filename'] = data[3]
+            item['electricity_save_date'] = data[4]
+            item['modem_cd'] = data[5]
+            item['modem_filename'] = data[6]
 
-    except:
-        itemid = 1
-
-    # 데이터를 삽입하는 sql을 실행
-    self.cursor.execute('insert into item values(%s, %s, %s, %s, %s)',
-                        (itemid, item['itemname'], item['price'],
-                         item['description'], item['pictureurl']))
-    # 삽입한 데이터를 데이터베이스에 반영
-    self.con.commit()
-    # 데이터베이스 접속 해제
-    self.close()
+        except self.cursor.Error as e:
+            # insert 실패시 False 반환
+            result = False
+            print(e)
+        self.close()
+        return item
