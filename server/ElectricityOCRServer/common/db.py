25c7cab569322495b08f61e0113d565ba6319d6d
2SEHI <58774664+2SEHI@users.noreply.github.com>
2021-10-05 19:14:59 +0900

    sql 수정, 메소드 추가
    1.리스트와 상세페이지 sql 수정
    2.이미지 등록 메소드 추가

--- server/ElectricityOCRServer/common/db.py
+++ server/ElectricityOCRServer/common/db.py
@@ -1,4 +1,5 @@
 import pymysql
+import sqlite3
 from flask import g
 
 class Dao:
@@ -10,7 +11,7 @@ class Dao:
         electricity_meter_tb.electricity_filename as electricity_filename, 
         date_format(electricity_meter_tb.electricity_save_date, '%Y-%m-%d %H:%i:%s') as electricity_save_date 
         from electricity_meter_tb 
-        join modem_tb 
+        left join modem_tb 
         on modem_tb.serial_cd = electricity_meter_tb.serial_cd 
         where electricity_meter_tb.del_flag = 0
         '''
@@ -23,34 +24,46 @@ class Dao:
         electricity_meter_tb.electricity_filename as electricity_filename, 
         date_format(electricity_meter_tb.electricity_save_date, '%%Y-%%m-%%d %%H:%%i:%%s') as electricity_save_date,  
         modem_tb.modem_cd as modem_cd, 
-        modem_tb.modem_filename as modem_filename 
+        modem_tb.modem_filename as modem_filename,
+        date_format(modem_tb.modem_save_date, '%%Y-%%m-%%d %%H:%%i:%%s') as modem_save_date  
         from electricity_meter_tb 
-        join modem_tb 
+        left join modem_tb 
         on modem_tb.serial_cd = electricity_meter_tb.serial_cd  
         where electricity_meter_tb.del_flag = 0 
-        and electricity_meter_tb.serial_cd = %s 
+        and electricity_meter_tb.serial_cd =  %s 
         '''
 
-        self.__SELECT_PRE_FILENAME = '''
+        self.__SELECT_PRE_IMAGE = '''
         select 
-        elect_pre_tb.pre_filename as pre_filename
-        from electricity_meter_tb as elect_tb
-        join electricity_preprocessing_tb as elect_pre_tb
-        on elect_tb.serial_cd = elect_pre_tb.serial_cd 
-        where elect_tb.del_flag = 0
-        and elect_tb.serial_cd = %s
+        electricity_preprocessing_tb.pre_filename as pre_filename
+        from electricity_meter_tb 
+        join electricity_preprocessing_tb 
+        on electricity_preprocessing_tb.serial_cd = electricity_meter_tb.serial_cd
+        where electricity_meter_tb.del_flag = 0
+        and electricity_meter_tb.serial_cd = %s
+        '''
+
+        self.__SELECT_BY_SERIAL_CD = '''
+        select serial_cd 
+        from electricity_meter_tb 
+        where serial_cd = %s
         '''
     #  데이터베이스 연결 메소드
     def connect(self):
-        # 연결
-        self.con = pymysql.connect(host='localhost',
-                                   port=3306,
-                                   user='flaskServer',
-                                   passwd='20210420',
-                                   db='electricitydb',
-                                   charset='utf8')
-        # 데이터베이스 사용 객체 생성
-        self.cursor = self.con.cursor()
+        try :
+            # 연결
+            self.con = pymysql.connect(host='localhost',
+                                       port=3306,
+                                       user='flaskServer',
+                                       passwd='20210420',
+                                       db='electricitydb',
+                                       charset='utf8')
+            # 데이터베이스 사용 객체 생성
+            self.cursor = self.con.cursor()
+        except Exception as err:
+            print('DBConnection Error : ', err)
+            return False
+        return True
 
     # 데이터베이스 연결 해제 메소드
     def close(self):
@@ -61,13 +74,15 @@ class Dao:
         # 데이터베이스 연결
         result = True
         li = []
+        connect_result = self.connect()
+        if not connect_result:
+            return connect_result, li
         try:
-            self.connect()
+
             # sql 문 실행
             self.cursor.execute(self.__SELECT_ALL)
             data = self.cursor.fetchall()
             # 데이터를 저장할 list
-
             for temp in data:
                 item = {}
                 item['serial_cd'] = temp[0]
@@ -76,46 +91,124 @@ class Dao:
                 item['electricity_save_date'] = temp[3]
                 li.append(item)
 
-        except Exception as e:
+
+        except self.cursor.Error as err:
             # insert 실패시 False 반환
             result = False
+            print('MySQL Error <select_all> : ', err)
         self.close()
         return result, li
 
     # dict형태로 데이터를 받아서 삽입하는 메소드
     def select_one(self, serial_cd):
+        print("server 들어옴")
         result = True
         item = {}
+        li = []
+        connect_result = self.connect()
+        if not connect_result:
+            return connect_result, li
         try:
-            self.connect()
-            
-            # 제조코드에 해당하는 전력량계량기와 모뎀코드 가져오기
-            self.cursor.execute(self.__SELECT_ONE, str(serial_cd))
+
+            self.cursor.execute(self.__SELECT_ONE, serial_cd)
             data = self.cursor.fetchone()
-            # 데이터가 존재하는 경우, 데이터를 json형식으로 설정
-            if len(data) > 0:
+            print("상세데이터 출력 : ", data)
+            item['serial_cd'] = data[0]
+            item['supply_type'] = data[1]
+            item['typename'] = data[2]
+            item['electricity_filename'] = data[3]
+            item['electricity_save_date'] = data[4]
+            item['modem_cd'] = data[5]
+            item['modem_filename'] = data[6]
+            item['modem_save_date'] = data[7]
 
-                item['serial_cd'] = data[0]
-                item['supply_type'] = data[1]
-                print(data[1])
-                item['typename'] = data[2]
+            self.cursor.execute(self.__SELECT_PRE_IMAGE, serial_cd)
+            imageList = self.cursor.fetchall()
 
-                item['electricity_filename'] = data[3]
-                item['electricity_save_date'] = data[4]
-                item['modem_cd'] = data[5]
-                item['modem_filename'] = data[6]
-                
-                # 제조코드에 해당하는 전처리 이미지파일명 검색
-                self.cursor.execute(self.__SELECT_PRE_FILENAME, str(serial_cd))
-                filenames = self.cursor.fetchall()
-                print(filenames)
-                temp = []
-                for filename in filenames:
-                    temp.append(filename[0])
-                item['pre_filenames'] = temp
-        except self.cursor.Error as e:
+            images = []
+            for temp in imageList:
+
+                images.append(temp[0])
+            item['pre_filenames'] = images
+            li.append(item)
+        except self.cursor.Error as err:
             # insert 실패시 False 반환
             result = False
-            print(e)
+            print('MySQL Error <select_one> : ', err)
         self.close()
         return result, item
+
+    # 계량기 정보 입력
+    def insert_meter(self, meter):
+        # 결과를 저장할 변수
+        result = False
+        self.connect()
+
+        data = self.cursor.execute(self.__SELECT_BY_SERIAL_CD ,
+                                   (meter['serial_cd']))
+        print('data::', type(data), ' ', data)
+
+        if data == 0:
+            # data = self.cursor.fetchone()
+            # itemid = 1
+            # # 데이터가 존재하는 경우는 가장 큰 itemid+1
+            # if data[0] != None:
+            #     itemid = int(data[0]) + 1
+            try:
+
+                self.cursor.execute('insert into electricity_meter_tb ' +
+                                    '(serial_cd, supply_type, typename,' +
+                                    ' electricity_filename, region_cd) ' +
+                                    'values(%s,%s,%s,%s,%s)',
+                                    (meter['serial_cd'], meter['supply_type'], meter['typename'],
+                                     meter['electricity_filename'], meter['region_cd']))
+                # 성공여부 확인 rowcount는 영향받은 행의 개수
+                if self.cursor.rowcount >= 1:
+                    result = True
+
+            except Exception as e:
+                print(e)
+                result = False
+
+        self.con.commit()
+        self.close()
+
+        return result
+
+        # 바코드 정보 입력
+
+    def insert_barcode(self, barcode):
+        # 결과를 저장할 변수
+        result = False
+        self.connect()
+
+        data = self.cursor.execute('select serial_cd from electricity_meter_tb where serial_cd = %s',
+                                   (barcode['serial_cd']))
+        print('data::', type(data), ' ', data)
+
+        # self.cursor.execute('select max(itemid) from item')
+        # data = self.cursor.fetchone()
+        # itemid = 1
+        # # 데이터가 존재하는 경우는 가장 큰 itemid+1
+        # if data[0] != None:
+        #     itemid = int(data[0]) + 1
+        print("DAO::", barcode)
+        if data == 1:
+            try:
+
+                self.cursor.execute('insert into modem_tb ' +
+                                    '(modem_cd, serial_cd, modem_filename) ' +
+                                    'values(%s,%s,%s)',
+                                    (barcode['modem_cd'], barcode['serial_cd'], barcode['modem_filename']))
+                # 성공여부 확인 rowcount는 영향받은 행의 개수
+                if self.cursor.rowcount >= 1:
+                    result = True
+
+            except Exception as e:
+                print(e)
+                result = False
+
+        self.con.commit()
+        self.close()
+
+        return result
